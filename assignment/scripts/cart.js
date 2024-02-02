b01ecaf186ea089c72dd945072e6a21124efeaed
lons <lonsnw@gmail.com>
2024-02-01 21:00:56 -0600

    Updating everything except the last one

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,15 +2,108 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
 
+// function addItem(item) {
+//     //how would we write this if we wanted to be able to add multiple things at once?
+//     basket.push(item);
+//     if (basket[basket.length-1] === item) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
 
+// }
 
+const maxItems = 5;
 
+function addItem(thing) {
+    //version 2
+    if (basket.length < maxItems) {
+        basket.push(thing);
+    }
+    if (basket[basket.length-1] === thing) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    
+}
 
+addItem(`tomatoes`);
+console.log(basket);
+console.log(addItem(`oranges`));
+console.log(addItem(`peppers`));
+console.log(basket);
 
+function listItems() {
+    for(food of basket){
+        console.log(food);
+    }
+}
 
+listItems();
 
+function empty(){
+    while(basket.length > 0) {
+        basket.pop();
+        console.log(basket);
+        }
+}
 
+empty();
+
+// const maxItems = 5;
+
+function isFull() {
+    if (basket.length < maxItems) {
+        return false;
+    }
+    else if (basket.length >= maxItems) {
+        return true;
+    }
+}
+
+addItem(`poptarts`);
+addItem(`candy`);
+addItem(`bagels`);
+addItem(`bananas`);
+addItem(`ketchup`);
+console.log(basket);
+console.log(`is the basket full?`, isFull());
+
+console.log(`can i add mints?`, addItem(`mints`));
+console.log(basket);
+
+empty();
+addItem(`candy`);
+addItem(`bagels`);
+addItem(`bananas`);
+addItem(`ketchup`);
+console.log(basket);
+console.log(`is the basket full?`, isFull());
+
+console.log(`can i add mints?`, addItem(`mints`));
+console.log(basket);
+
+function removeItem(item) {
+    basket.indexOf(item);
+    console.log(basket.indexOf(item));
+    //works up to here 
+    console.log(basket.splice(basket.indexOf(item), 1));
+    //i don't know how to get it to return null.  the below doesn't work.
+    if (basket.indexOf(item) >= 0) {
+        return item;
+    }
+    else if (basket.indexOf(item) === -1) {
+        return null
+    }
+}
+
+console.log(removeItem(`mustard`));
+console.log(removeItem(`mints`));
 
 // DO NOT MODIFY
 // Used for automated testing
